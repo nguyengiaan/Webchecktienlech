b23e690a3de2da401afb0ef25af05c701b609cf4
Admin <Admin@ADMIN-PC>
2025-04-04 08:39:57 +0700

    Add project files.

---
+++ Webchecktienlech/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Webchecktienlech.Models;
+
+namespace Webchecktienlech.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/So-sanh-ho-so")]
+        public IActionResult Sosanhhoso()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
